55c4a90379a551f1998451652811c3bb1b670666
Davidf77749 <davidfinck2@gmail.com>
2020-10-09 22:54:18 -0400

    finished part 1a

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -1,1 +1,24 @@
 
+
+function iterating(tag, indent) {
+  var i;
+  var l;
+  for (i = 0; i < indent; i++) {
+    document.getElementById("info").innerHTML += "-";
+  }
+
+  document.getElementById("info").innerHTML += tag.tagName + "\n";
+
+  var amount = tag.childElementCount;
+  
+  if (amount > 0) {
+    var j = 0;
+    for (j = 0; j < amount; j++) {
+      iterating(tag.children[j], indent + 1);
+    }
+  }
+
+  return;
+}
+
+
