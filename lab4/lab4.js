0f2eb22afa8cf210350997544e5e556e99830384
zl20144 <55772447+zl20144@users.noreply.github.com>
2020-10-12 22:23:07 -0400

    Add files via upload

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -1,30 +1,63 @@
+/* Global Variables */
+var part1a = "";
+var part1b = "";
 
-
-function iterating(tag, indent) {
+/* Functions / Methods */
+function iterating1a(tag, indent) {
+  var result = "";
   var i;
-  var l;
   if(indent == 0){
-      document.getElementById("info").innerHTML = "";
+      result = "";
   }
   for (i = 0; i < indent; i++) {
-    document.getElementById("info").innerHTML += "-";
+    result += "-";
   }
 
-  document.getElementById("info").innerHTML += tag.tagName + "\n";
+  result += (tag.tagName + "\n");
 
   var amount = tag.childElementCount;
   
   if (amount > 0) {
     var j = 0;
     for (j = 0; j < amount; j++) {
-      iterating(tag.children[j], indent + 1);
+      result += iterating1a(tag.children[j], indent + 1);
     }
   }
 
-  return;
+  return result;
 }
 
-var x = document.getElementsByTagName("html").item(0);
-/*document.getElementById("info").innerHTML += "\n";*/
-iterating(x, 0);
+function iterating1b(tag, indent) {
+  var result = "";
+  var i;
+  if(indent == 0){
+      result = "";
+  }
+  for (i = 0; i < indent; i++) {
+    result += "-";
+  }
 
+  result += (tag.tagName + "<br/>");
+
+  var amount = tag.childElementCount;
+  
+  if (amount > 0) {
+    var j = 0;
+    for (j = 0; j < amount; j++) {
+      result += iterating1b(tag.children[j], indent + 1);
+    }
+  }
+
+  return result;
+}
+/* Part 1a execution */
+var x = document.getElementsByTagName("html").item(0);
+document.getElementById("info").innerHTML = "";
+part1a = iterating1a(x, 0);
+document.getElementById("info").innerHTML += part1a;
+
+/* Part 1b execution */
+var y = document.getElementsByClassName("quote");
+part1b = iterating1b(y[0], 0);
+document.getElementById("part1b").innerHTML = "";
+document.getElementById("part1b").innerHTML += part1b;
