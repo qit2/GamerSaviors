064cb4f9c4e10535cbf0903a2983603b4b5cc6c4
qit2 <55993238+qit2@users.noreply.github.com>
2020-10-12 23:42:27 -0400

    Update lab4.js

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -1,44 +1,73 @@
-function iterating(tag, indent) {
+/* Global Variables */
+var part1a = "";
+var part1b = "";
+
+/* Functions / Methods */
+function iterating1a(tag, indent) {
+  var result = "";
   var i;
-  var l;
+  if(indent == 0){
+      result = "";
+  }
   for (i = 0; i < indent; i++) {
-    document.getElementById("info").innerHTML += "-";
+    result += "-";
   }
 
-  document.getElementById("info").innerHTML += tag.tagName + "\n";
+  result += (tag.tagName + "\n");
 
   var amount = tag.childElementCount;
   
   if (amount > 0) {
     var j = 0;
     for (j = 0; j < amount; j++) {
-      iterating(tag.children[j], indent + 1);
+      result += iterating1a(tag.children[j], indent + 1);
     }
   }
 
-  return;
+  return result;
 }
 
-function iterating2(tag, indent) {
+function iterating1b(tag, indent) {
+  var result = "";
   var i;
-  var l;
+  if(indent == 0){
+      result = "";
+  }
   for (i = 0; i < indent; i++) {
-    document.getElementById("part1b").innerHTML += "-";
+    result += "-";
   }
 
-  document.getElementById("part1b").innerHTML += tag.tagName + "\n";
+  result += (tag.tagName + "<br/>");
 
   var amount = tag.childElementCount;
   
   if (amount > 0) {
     var j = 0;
     for (j = 0; j < amount; j++) {
-      iterating2(tag.children[j], indent + 1);
+      result += iterating1b(tag.children[j], indent + 1);
     }
   }
 
-  return;
+  return result;
 }
+/* Part 1a execution */
+var x = document.getElementsByTagName("html").item(0);
+document.getElementById("info").innerHTML = "";
+part1a = iterating1a(x, 0);
+document.getElementById("info").innerHTML += part1a;
 
+/* Part 1b execution */
+var y = document.getElementsByClassName("quote");
+part1b = iterating1b(y[0], 0);
+document.getElementById("part1b").innerHTML = "";
+document.getElementById("part1b").innerHTML += part1b;
 
-
+function button_click(){
+  var content = document.getElementById("part1b");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+  }
+  else{
+    content.style.display = "none";
+  }
+}
