a3e7e7d488bc66adc445bd23014411d6ea7b2d38
Davidf77749 <davidfinck2@gmail.com>
2020-10-25 19:00:03 -0400

    added the three sliders to the game

--- lab5/hexed.js
+++ lab5/hexed.js
@@ -1,1 +1,95 @@
 
+(function ( $ ) {
+ 
+  $.fn.hexed = function( settings ) {
+
+    function dectohex(num) {
+      return num.toString(16).toUpperCase();
+    }
+
+    function hextodec(num) {
+      return parseInt(num, 16);
+    }
+    
+
+
+    //This function is the three colored sliders for the game
+    $( function() {
+
+      //This is the red slider
+      $( "#slider-horR" ).slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 0,
+        max: 255,
+        value: 0,
+        slide: function( event, ui ) {
+          $( "#amountR" ).val( ui.value );
+        }
+      });
+      $('#amountR').change(function(){
+        var value = this.value.substring(0);
+        console.log(value);
+        $("#slider-horR").slider("value", parseInt(value));
+      });
+
+      $( "#slider-horR" ).css('background', 'rgb(255,204,203)');
+      $( "#slider-horR .ui-slider-range" ).css('background', 'rgb(255,0,0)');
+      $( "#amountR" ).val( $( "#slider-horR" ).slider( "value" ) );
+
+
+      //This is the green slider
+      $( "#slider-horG" ).slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 0,
+        max: 255,
+        value: 0,
+        slide: function( event, ui ) {
+          $( "#amountG" ).val( ui.value );
+        }
+      });
+
+      $('#amountG').change(function(){
+        var value = this.value.substring(0);
+        console.log(value);
+        $("#slider-horG").slider("value", parseInt(value));
+      });
+
+
+      $( "#slider-horG" ).css('background', 'rgb(144,238,144)');
+      $( "#slider-horG .ui-slider-range" ).css('background', 'rgb(11,140,35)');
+      $( "#amountG" ).val( $( "#slider-horG" ).slider( "value" ) );
+
+
+      //This is the blue slider
+      $( "#slider-horB" ).slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 0,
+        max: 255,
+        value: 0,
+        slide: function( event, ui ) {
+          $( "#amountB" ).val( ui.value );
+        }
+      });
+
+      $('#amountB').change(function(){
+        var value = this.value.substring(0);
+        console.log(value);
+        $("#slider-horB").slider("value", parseInt(value));
+      });
+
+      $( "#amountB" ).val( $( "#slider-horB" ).slider( "value" ) );
+      $( "#slider-horB" ).css('background', '#03a9f4');
+      $( "#slider-horB .ui-slider-range" ).css('background', 'rgb(0,32,255)');
+    } );
+
+  };
+
+  return this;
+
+}( jQuery ));
+
+var settings = { name: "John", turns: 3 };
+$("#game").hexed(settings);
