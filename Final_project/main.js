5e5fcd8899d0a1d1ec6af32144c805701adbfb9b
Davidf77749 <davidfinck2@gmail.com>
2020-12-02 17:05:06 -0600

    I fixed a few minor issues

--- Final_project/main.js
+++ Final_project/main.js
@@ -21,3 +21,93 @@ function prankrestored(){
     document.getElementById("about").innerHTML = "About Us";
     document.getElementById("cyan").style.display = "none";
 }
+
+function myFunction1() {
+  var x = document.getElementById("rating");
+  var y = document.getElementById("Rrating");
+  var z = document.getElementById("price");
+  var a = document.getElementById("Rprice");
+  var b = document.getElementById("alpha");
+  var c = document.getElementById("Ralpha");
+  x.style.display = "block";
+  y.style.display = "none";
+  z.style.display = "none";
+  a.style.display = "none";
+  b.style.display = "none";
+  c.style.display = "none";
+}
+
+function myFunction2() {
+  var x = document.getElementById("rating");
+  var y = document.getElementById("Rrating");
+  var z = document.getElementById("price");
+  var a = document.getElementById("Rprice");
+  var b = document.getElementById("alpha");
+  var c = document.getElementById("Ralpha");
+  x.style.display = "none";
+  y.style.display = "block";
+  z.style.display = "none";
+  a.style.display = "none";
+  b.style.display = "none";
+  c.style.display = "none";
+}
+
+function myFunction3() {
+  var x = document.getElementById("rating");
+  var y = document.getElementById("Rrating");
+  var z = document.getElementById("price");
+  var a = document.getElementById("Rprice");
+  var b = document.getElementById("alpha");
+  var c = document.getElementById("Ralpha");
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "block";
+  a.style.display = "none";
+  b.style.display = "none";
+  c.style.display = "none";
+}
+
+function myFunction4() {
+  var x = document.getElementById("rating");
+  var y = document.getElementById("Rrating");
+  var z = document.getElementById("price");
+  var a = document.getElementById("Rprice");
+  var b = document.getElementById("alpha");
+  var c = document.getElementById("Ralpha");
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "none";
+  a.style.display = "block";
+  b.style.display = "none";
+  c.style.display = "none";
+}
+
+function myFunction5() {
+  var x = document.getElementById("rating");
+  var y = document.getElementById("Rrating");
+  var z = document.getElementById("price");
+  var a = document.getElementById("Rprice");
+  var b = document.getElementById("alpha");
+  var c = document.getElementById("Ralpha");
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "none";
+  a.style.display = "none";
+  b.style.display = "block";
+  c.style.display = "none";
+}
+
+function myFunction6() {
+  var x = document.getElementById("rating");
+  var y = document.getElementById("Rrating");
+  var z = document.getElementById("price");
+  var a = document.getElementById("Rprice");
+  var b = document.getElementById("alpha");
+  var c = document.getElementById("Ralpha");
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "none";
+  a.style.display = "none";
+  b.style.display = "none";
+  c.style.display = "block";
+}
